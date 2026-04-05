df08154b6877e213d1c1cfca3395029e0859d96b
xyrildvonne-git <xyrildvonne.tee@benilde.edu.ph>
2026-04-05 15:22:52 +0800

    Create Program.cs
    created this so i can call Card Data Source

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using ConsoleApp4;
+
+class Program
+{
+    static void Main()
+    {
+        List<Card> allCards = DataSource.LoadCards(@"C:\Users\admin\Documents\cards.txt"); //i placed the full path; can be changed
+
+        Console.WriteLine("Cards loaded: " + allCards.Count);
+    }
+
+    public static Dictionary<string, string> AttackNames = new Dictionary<string, string>
+{
+    {"JMS", "Jump Scare"},
+    {"SFB", "Soft Block"},
+    {"HPS", "Hard Pass"},
+    {"DLL", "Delulu"},
+    {"BRR", "Brain Rot"},
+    {"MXS", "Mixed Signal"},
+    {"RBD", "Rebound"},
+    {"ORB", "Orbiting"},
+    {"RLP", "Relapse"},
+    {"GHS", "Ghosting"},
+    {"HRL", "Hard Launch"},
+    {"NCH", "Nonchalant"},
+    {"LVB", "Love Bomb"},
+    {"AUF", "Aura Farm"},
+    {"TRD", "Trauma Dump"}
+};
+
+}
