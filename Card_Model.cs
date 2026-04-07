b89a58f53046a9d4fbf4c70899f84a67b2b156a0
EL <layuganmishael11@gmail.com>
2026-04-08 04:56:59 +0800

    Modified Card_Model still missing ASCII Logics

--- Card_Model.cs
+++ Card_Model.cs
@@ -1,26 +1,74 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ConsoleApp4
+namespace Card_Model
 {
-    internal class Card_Model
+
+    public enum CardType
     {
+        FIRE,
+        WATER,
+        GRASS
+    }
 
-        public abstract class Card
+    public interface ICard
+    {
+        string Name { get; set; }
+        CardType Type { get; set; }
+        int HP { get; set; }
+    }
+
+    public abstract class BaseCard : ICard  //Battle Cards
+    {
+        public string Name { get; set; }
+        public CardType Type { get; set; }
+        public int Rarity { get; set; }
+        public int HP { get; set; }
+        public int AttackCode { get; set; }
+        public int Attack { get; set; }
+        public string SpecialMove { get; set; }
+
+        public abstract void DisplayCard();
+        public abstract void PullCard();
+
+    }
+
+    public class FireCard : BaseCard
+    {
+        public override void DisplayCard()
         {
-            public string Name { get; set; }
-            public string Type { get; set; }
-            public int HP { get; set; }
-            public int AttackDamage { get; set; }
-            public int Rarity { get; set; }
-            public string SpecialMove { get; set; }
-
-            public abstract void DisplayCard();
-            public abstract void PullCard();
 
         }
 
-    }
-}
+        public override void PullCard()
+        {
 
+        }
+    }
+
+    public class WaterCard : BaseCard
+    {
+        public override void DisplayCard()
+        {
+
+        }
+
+        public override void PullCard()
+        {
+
+        }
+    }
+
+    public class GrassCard : BaseCard
+    {
+        public override void DisplayCard()
+        {
+
+        }
+
+        public override void PullCard()
+        {
+
+        }
+    }
+
+}
