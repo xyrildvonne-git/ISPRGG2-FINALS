733273d3588a13515aaaa29321ae90f57ad1e4a3
EL <layuganmishael11@gmail.com>
2026-04-10 04:02:15 +0800

    New class for Binder System

---
+++ BinderSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Card_Model;
+
+namespace ConsoleApp4
+{
+    public class BinderSystem
+    {
+
+        public static List<BaseCard> ownedCards = new List<BaseCard>(); //storage for owned cards
+
+
+        public static void AddToBinder(BaseCard card) // updates the binder for new cards
+        {
+            if (!ownedCards.Any(c => c.Name == card.Name))
+            {
+                ownedCards.Add(card);
+            }
+        }
+
+
+        public static bool IsCardUnlocked(string cardName) // tells the printer if a card is unlocked
+        {
+            return ownedCards.Any(c => c.Name == cardName);
+        }
+    }
+}
