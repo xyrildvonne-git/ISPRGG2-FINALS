4dda7b3b407f9a393e90d040a198177e8543062d
xyrildvonne-git <xyrildvonne.tee@benilde.edu.ph>
2026-04-11 12:33:35 +0800

    Updated namespace and added save and load cards here

--- Program.cs
+++ Program.cs
@@ -1,62 +1,66 @@
-using ConsoleApp4;
-
 using System;
 using System.Collections.Generic;
 
-namespace ConsoleApp4
+namespace UltraMoonTCG;
+
+internal class Program
 {
-    internal class Program
+    public static List<BaseCard> allCards;
+
+    public static Dictionary<string, string> AttackNames = new Dictionary<string, string>
     {
-        public static List<CardPrint> allCards;
+        {"JMS", "Jump Scare"},
+        {"SFB", "Soft Block"},
+        {"HPS", "Hard Pass"},
+        {"DLL", "Delulu"},
+        {"BRR", "Brain Rot"},
+        {"MXS", "Mixed Signal"},
+        {"RBD", "Rebound"},
+        {"ORB", "Orbiting"},
+        {"RLP", "Relapse"},
+        {"GHS", "Ghosting"},
+        {"HRL", "Hard Launch"},
+        {"NCH", "Nonchalant"},
+        {"LVB", "Love Bomb"},
+        {"AUF", "Aura Farm"},
+        {"TRD", "Trauma Dump"}
+    };
 
-        public static Dictionary<string, string> AttackNames = new Dictionary<string, string>
+    static void Main()
+    {
+        // Loads cards.txt
+        allCards = DataSource.LoadCards("cards.txt");
+
+        // Loads pulled cards
+        CardPulls.Load(allCards);
+
+
+        GameScreen currentScreen = new MainMenu();
+
+        while (currentScreen != null)
         {
-            {"JMS", "Jump Scare"},
-            {"SFB", "Soft Block"},
-            {"HPS", "Hard Pass"},
-            {"DLL", "Delulu"},
-            {"BRR", "Brain Rot"},
-            {"MXS", "Mixed Signal"},
-            {"RBD", "Rebound"},
-            {"ORB", "Orbiting"},
-            {"RLP", "Relapse"},
-            {"GHS", "Ghosting"},
-            {"HRL", "Hard Launch"},
-            {"NCH", "Nonchalant"},
-            {"LVB", "Love Bomb"},
-            {"AUF", "Aura Farm"},
-            {"TRD", "Trauma Dump"}
-        };
-
-        static void Main()
-        {
-            // Load cards
-            allCards = DataSource.LoadCards("card.txt");
-
-            // Start menu
-            Screen currentScreen = new MainMenu();
-
-            while (currentScreen != null)
-            {
-
-                currentScreen.DisplayUI();
-                currentScreen = currentScreen.ProcessInput();
-            }
-
-            // Shutdown screen
-            Console.Clear();
-            Screen.InitializeScreen("CREDITS");
-            Console.WriteLine("Thank you so much for playing!");
-
-            Console.WriteLine("\nGame made by:");
-            Console.WriteLine("De Guzman, Kellie");
-            Console.WriteLine("Layugan, Mishael");
-            Console.WriteLine("Tee, Xyril");
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\n[X] Press any key to exit.");
-            Console.ResetColor();
-            Console.ReadKey(true);
+            currentScreen.DisplayUI();
+            currentScreen = currentScreen.ProcessInput();
         }
+
+        // Saves cards before exit
+        CardPulls.Save(allCards);
+
+
+        // Shutdown screen
+        Console.Clear();
+        GameScreen.InitializeScreen("CREDITS");
+
+        Console.WriteLine("Thank you so much for playing!");
+
+        Console.WriteLine("\nGame made by:");
+        Console.WriteLine("De Guzman, Kellie");
+        Console.WriteLine("Layugan, Mishael");
+        Console.WriteLine("Tee, Xyril");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\n[X] Press any key to exit.");
+        Console.ResetColor();
+        Console.ReadKey(true);
     }
 }
