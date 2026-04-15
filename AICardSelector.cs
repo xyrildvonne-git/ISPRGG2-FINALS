b2191b6ab2c29c970c0ec872c6e90e33f03853f5
Swinteroo <37173604+Swinteroo@users.noreply.github.com>
2026-04-16 01:07:43 +0800

    Renamed SelectAICard and DisplayAIcard and used \n's
    - Renamed SelectCard to SelectAICard for readability
    - Renamed DisplayCard to DisplayAICard for readability
    - Replaced Console.WriteLine's with \n's

--- AICardSelector.cs
+++ AICardSelector.cs
@@ -6,7 +6,7 @@ public class AICardSelector
 
     public static BaseCard ActiveSlot { get; set; }
 
-    public static void SelectCard()
+    public static void SelectAICard()
     {
 
         if (Program.allCards.Count == 0) // Checks if the AI has the list
@@ -22,12 +22,10 @@ public class AICardSelector
         ActiveSlot = Program.allCards[randomIndex]; //places the random card to the ai active slot
     }
 
-    public static void DisplayCard()
+    public static void DisplayAICard()
     {
-        Console.WriteLine("Your opponent has chosen!");
-        Console.WriteLine();
-        ActiveSlot.PrintCard();
-        Console.WriteLine();
-        Console.WriteLine($"AI will battle with {ActiveSlot.Name}!");
+        Console.WriteLine("Your opponent has chosen!\n");
+        ActiveSlot.PrintColoredCard();
+        Console.WriteLine($"\nAI will battle with {ActiveSlot.Name}!");
     }
 }
