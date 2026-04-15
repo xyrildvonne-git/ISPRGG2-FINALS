e790af011a28f56c062edf39933f1f54ecfcaae8
xyrildvonne-git <xyrildvonne.tee@benilde.edu.ph>
2026-04-16 01:51:22 +0800

    added a new method called in BinderSystem
    CardPrinter class is now the sole printer of cards to avoid redundancy and easier fix

--- CardPrinter.cs
+++ CardPrinter.cs
@@ -1,89 +1,92 @@
-namespace UltraMoonTCG;
-
-public class CardPrinter
+namespace UltraMoonTCG
 {
-
-    public static void PrintCard(BaseCard card, bool showStack = false)
+    public class CardPrinter
     {
-        Console.ForegroundColor = card.ElementColor;
-        string borderTop = "";
-        string borderBottom = "";
-        char sideChar = '|';
-
-        switch (card.Rarity)
+        // new method so BinderSystem can call the borders (card format) to avoid redundancy
+        public static (string borderTop, string borderBottom, char sideChar, string rightSide)
+            GetBorders(BaseCard card, bool showStack)
         {
-            case RarityLevel.Common:
-                borderTop = borderBottom = "+------------------+";
-                sideChar = '|';
-                break;
+            string borderTop = "";
+            string borderBottom = "";
+            char sideChar = '|';
 
-            case RarityLevel.Uncommon:
-                borderTop = borderBottom = "+==================+";
-                sideChar = '|';
-                break;
+            switch (card.Rarity)
+            {
+                case RarityLevel.Common:
+                    borderTop = borderBottom = "+-------------------+";
+                    sideChar = '|';
+                    break;
+                case RarityLevel.Uncommon:
+                    borderTop = borderBottom = "+===================+";
+                    sideChar = '|';
+                    break;
+                case RarityLevel.Rare:
+                    borderTop = borderBottom = "*********************";
+                    sideChar = '*';
+                    break;
+                case RarityLevel.Epic:
+                    borderTop = borderBottom = "@@@@@@@@@@@@@@@@@@@@@";
+                    sideChar = '@';
+                    break;
+                case RarityLevel.Legendary:
+                    borderTop = borderBottom = "#####################";
+                    sideChar = '#';
+                    break;
+            }
 
-            case RarityLevel.Rare:
-                borderTop = borderBottom = "********************";
-                sideChar = '*';
-                break;
+            string rightSide = sideChar.ToString();
 
-            case RarityLevel.Epic:
-                borderTop = borderBottom = "@@@@@@@@@@@@@@@@@@@@";
-                sideChar = '@';
-                break;
+            if (showStack)
+            {
+                if (card.PullCount == 2)
+                {
+                    borderTop += new string(borderTop.Last(), 1);
+                    borderBottom += new string(borderBottom.Last(), 1);
+                    rightSide = new string(sideChar, 2);
+                }
+                else if (card.PullCount == 3)
+                {
+                    borderTop += new string(borderTop.Last(), 2);
+                    borderBottom += new string(borderBottom.Last(), 2);
+                    rightSide = new string(sideChar, 3);
+                }
+                else if (card.PullCount == 4)
+                {
+                    borderTop += new string(borderTop.Last(), 3);
+                    borderBottom += new string(borderBottom.Last(), 3);
+                    rightSide = new string(sideChar, 4);
+                }
+                else if (card.PullCount >= 6)
+                {
+                    borderTop += $" x{card.PullCount}";
+                    rightSide = new string(sideChar, 1);
+                }
+            }
 
-            case RarityLevel.Legendary:
-                borderTop = borderBottom = "####################";
-                sideChar = '#';
-                break;
+            return (borderTop, borderBottom, sideChar, rightSide);
         }
 
-        string rightSide = sideChar.ToString();
-
-        // Adjusts borders based on pull count
-        if (showStack == true)
+        public static void PrintCard(BaseCard card, bool showStack = false)
         {
-            if (card.PullCount == 2)
-            {
-                borderTop += new string(borderTop.Last(), 1);
-                borderBottom += new string(borderBottom.Last(), 1);
-                rightSide = new string(sideChar, 2);
-            }
-            else if (card.PullCount == 3)
-            {
-                borderTop += new string(borderTop.Last(), 2);
-                borderBottom += new string(borderBottom.Last(), 2);
-                rightSide = new string(sideChar, 3);
-            }
-            else if (card.PullCount == 4)
-            {
-                borderTop += new string(borderTop.Last(), 3);
-                borderBottom += new string(borderBottom.Last(), 3);
-                rightSide = new string(sideChar, 4);
-            }
-            else if (card.PullCount >= 6)
-            {
-                borderTop += $" x{card.PullCount}";
-                rightSide = new string(sideChar, 1);
-            }
-            else
-            {
-                rightSide = new string(sideChar, 1);
-            }
+            Console.ForegroundColor = card.ElementColor;
+
+            var (borderTop, borderBottom, sideChar, rightSide) = GetBorders(card, showStack);
+
+            string attackName = MoveLibrary.AttackNames.ContainsKey(card.AttackCode)
+                ? MoveLibrary.AttackNames[card.AttackCode]
+                : card.AttackCode;
+
+            Console.WriteLine(borderTop);
+            Console.WriteLine($"{sideChar} {card.Name.PadRight(10)} HP:{card.HP.ToString().PadLeft(3)} {rightSide}");
+            Console.WriteLine($"{sideChar} TYPE: {card.Type.ToString().PadRight(12)}{rightSide}");
+
+            string attackLine = $"{attackName}:";
+            Console.WriteLine($"{sideChar} {attackLine.PadRight(13)} {card.Attack.ToString().PadLeft(3)} {rightSide}");
+
+            Console.WriteLine($"{sideChar} {card.SpecialMove.PadRight(18)}{rightSide}");
+            Console.WriteLine(borderBottom);
+
+            Console.ResetColor();
         }
-
-        string attackName = MoveLibrary.AttackNames.ContainsKey(card.AttackCode)
-            ? MoveLibrary.AttackNames[card.AttackCode]
-            : card.AttackCode;
-
-        Console.WriteLine(borderTop);
-        Console.WriteLine($"{sideChar} {card.Name.PadRight(9)} HP:{card.HP.ToString().PadLeft(3)} {rightSide}");
-        Console.WriteLine($"{sideChar} TYPE: {card.Type.ToString().PadRight(11)}{rightSide}");
-        string attackLine = $"{attackName}:";
-        Console.WriteLine($"{sideChar} {attackLine.PadRight(12)} {card.Attack.ToString().PadLeft(3)} {rightSide}");
-        Console.WriteLine($"{sideChar} {card.SpecialMove.PadRight(17)}{rightSide}");
-        Console.WriteLine(borderBottom);
-
-        Console.ResetColor();
     }
 }
