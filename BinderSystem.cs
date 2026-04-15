8d279cc1624b4640d597f195cee19632fc37e732
xyrildvonne-git <xyrildvonne.tee@benilde.edu.ph>
2026-04-16 02:02:19 +0800

    calls the method in CardPrinter

--- BinderSystem.cs
+++ BinderSystem.cs
@@ -1,4 +1,3 @@
-using static UltraMoonTCG.BaseScreen;
 namespace UltraMoonTCG;
 
 public static class BinderSystem
@@ -15,19 +14,53 @@ public static class BinderSystem
         }
     }
 
-    public static void DisplayBinderCard(int index)
+    public static void ShowCard(int index)
     {
         BaseCard card = Program.allCards.FirstOrDefault(c => c.Index == index); // checks if the card exists and if the user owns it
 
         if (card == null || card.PullCount == 0) // determines whether a card is locked
         {
-            WriteColorLine("\n[LOCKED] You have not pulled this card yet.", ConsoleColor.Red);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n[LOCKED] You have not pulled this card yet.");
+            Console.ResetColor();
             return;
         }
 
         // Removed console clear
         Console.WriteLine();
+       // calls method in CardPrinter to print the card format in BinderSystem
+        var (borderTop, borderBottom, sideChar, rightSide) = CardPrinter.GetBorders(card, showStack: true);
+        CardPrinter.PrintCard(card, showStack: true);
 
-        CardPrinter.PrintCard(card, true);
+
+        // Adjusts borders based on pull count
+        if (card.PullCount == 2)
+        {
+            borderTop += new string(borderTop.Last(), 1);
+            borderBottom += new string(borderBottom.Last(), 1);
+            rightSide = new string(sideChar, 2);
+        }
+        else if (card.PullCount == 3)
+        {
+            borderTop += new string(borderTop.Last(), 2);
+            borderBottom += new string(borderBottom.Last(), 2);
+            rightSide = new string(sideChar, 3);
+        }
+        else if (card.PullCount == 4)
+        {
+            borderTop += new string(borderTop.Last(), 3);
+            borderBottom += new string(borderBottom.Last(), 3);
+            rightSide = new string(sideChar, 4);
+        }
+        else if (card.PullCount >= 6)
+        {
+            borderTop += $" x{card.PullCount}";
+            rightSide = new string(sideChar, 1);
+        }
+        else
+        {
+            rightSide = new string(sideChar, 1);
+        }
+
     }
 }
