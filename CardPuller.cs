3a152cd5fe7768722d3b98e332119441341b87ed
Swinteroo <37173604+Swinteroo@users.noreply.github.com>
2026-04-16 01:06:01 +0800

    Renamed Save as SavePulls
    - Renamed Save cards method as SavePulls for readability in PullCardScreen code

--- CardPuller.cs
+++ CardPuller.cs
@@ -5,7 +5,7 @@ public static class CardPuller
     // file path where pull data is stored
     private static string path = "pulls.txt";
 
-   // loads saved cards
+    // loads saved cards
     public static void Load(List<BaseCard> cards)
     {
         if (!File.Exists(path)) // exit if save file does not exist
@@ -23,14 +23,14 @@ public static class CardPuller
 
             BaseCard card = cards.Find(c => c.Index == index); // find matching card in memory using index
 
-           // if cards exist, restore its pull count
+            // if cards exist, restore its pull count
             if (card != null)
                 card.PullCount = pulls;
         }
     }
-   
+
     // saves pulled cards
-    public static void Save(List<BaseCard> cards)
+    public static void SavePulls(List<BaseCard> cards)
     {
         using (StreamWriter writer = new StreamWriter(path)) // create or overwrite saved data
         {
@@ -44,9 +44,9 @@ public static class CardPuller
             }
         }
     }
-   
+
     // deletes pulls.txt
-    public static void ResetSave(List<BaseCard> cards)
+    public static void ResetSavedPulls(List<BaseCard> cards)
     {
         if (File.Exists(path))
             File.Delete(path);
